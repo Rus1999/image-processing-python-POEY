36671d35d563dce81aee6082d23c5b4b4249dc38
Punyarit <punyarit07@gmail.com>
2022-07-04 17:15:54 +0700

    first commit

---
+++ week2/coding_test.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+num1 = 5
+print(num1)
+num2 = 5.5
+print(num2)
+str1 = "Punyarit Klaphachon"
+print(str1)
+print(num1, '\n', num2, '\t', str1)
+
---
+++ week2/test.py
@@ -0,0 +1,9 @@
+import cv2
+
+# img = cv2.imread('C:\Users\punya\Desktop\mementomori.jpg')
+img = cv2.imread("D:\workspace\python\picture\human-skull-with-crossed-bones-silhouette.png")
+
+cv2.imshow('sample image', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
