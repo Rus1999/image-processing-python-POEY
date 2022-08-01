063492582a15b25970e74cd55885392965868707
Punyarit <punyarit07@gmail.com>
2022-08-01 17:05:35 +0700

    add translation_image.py

---
+++ week6/translation_image.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy
+
+img = cv2.imread("D:\\workspace\\python\\week6\\img\\short.jpg")
+
+# transaltion matrix
+# [x], [y]
+M = numpy.float32([[1, 0, -200], [0, 1, -200]])
+h, w = img.shape[:2]
+
+translate = cv2.warpAffine(img, M, (w, h))
+
+cv2.imshow("Original", img)
+cv2.imshow("Original", translate)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
