8c090d400a0151a86007f43812aec953f4285775
Rus <punyarit07@gmail.com>
2022-10-10 15:44:24 +0700

    dilatation

---
+++ week16/dilation.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+ogImg = cv2.imread("R://workspace//image-processing-python-POEY//week16//images//textbw.jpg", 0)
+
+kernel = np.ones((5, 5), np.uint8)
+
+dilation = cv2.dilate(ogImg, kernel, iterations=1)
+
+cv2.imshow("OG", ogImg)
+cv2.imshow("Dilation", dilation)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
